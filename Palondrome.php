523c5c892250218b86a084ee82df4a6587c1667e
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 21:43:40 +0200

    php <=> manually increasing length

--- Palondrome.php
+++ Palondrome.php
@@ -8,7 +8,17 @@ if ($_SERVER['REQUEST_METHOD']== 'POST'){
 
         for ($i = 0;isset($word[$i]); $i++){
             $char = $word[$i];
-            
+            // lowercase any capital letter
+            if($char >= 'A' && $char <= 'Z'){
+                $processed.= $char +32;
+            }else{
+                $processed.=$char;
+            }
+        }
+
+        $length = 0;
+        while(isset($processed.$length)){
+            $length++;
         }
 
     }
