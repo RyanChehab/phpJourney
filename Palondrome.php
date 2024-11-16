80fac7373e94f3da074eaeb5fa4fe689b3e570d4
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 21:36:43 +0200

    php <=> checked the request method

---
+++ Palondrome.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD']== 'POST'){
+    $word = $_POST['word'];
+
+    function checker($word){
+        $processed = '';
+
+        for ($i = 0;isset($word[$i]); $i++){
+            $char = $word[$i];
+            
+        }
+
+    }
+}
