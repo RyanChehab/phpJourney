d64e2a0d20137002bfe4953852666e5c6508ee47
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 22:36:14 +0200

    php <=> created base case

---
+++ merge_sort.php
@@ -0,0 +1,16 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $array = $_POST['array'];
+    
+    function merge_sort($array){
+        // base case
+        if(count($array) <=1){
+            return $array;
+        }
+
+        // dividing the array
+        
+
+    }
+}
