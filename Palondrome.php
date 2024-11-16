c88b6a98d97db2cb154fcb712e9c6bd1a58b2d55
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 21:51:05 +0200

    php <=> checking if the word is palondrome

--- Palondrome.php
+++ Palondrome.php
@@ -15,10 +15,20 @@ if ($_SERVER['REQUEST_METHOD']== 'POST'){
                 $processed.=$char;
             }
         }
-
+        // calculating the length
         $length = 0;
-        while(isset($processed.$length)){
+        while(isset($processed[$length])){
             $length++;
+        }
+        // reversing the word
+        $reversed="";
+        for ($i = 0; $i<$length; $i++){
+            if($processed[$i] !== $reversed[$i]){
+                $response=[];
+                $response['status'] = 'Failed';
+                $response['massege'] = 'not a palondrome';
+                echo json_encode($response);
+            }
         }
 
     }
