99303c1b36596a72fe0718cc4b1e29fe316e9cd0
RyanChehab <ryanchehab20@gmail.com>
2024-11-16 22:17:46 +0200

    php <=> sending back response

--- Palondrome.php
+++ Palondrome.php
@@ -10,26 +10,37 @@ if ($_SERVER['REQUEST_METHOD']== 'POST'){
             $char = $word[$i];
             // lowercase any capital letter
             if($char >= 'A' && $char <= 'Z'){
-                $processed.= $char +32;
+                $processed.= chr(ord($char) +32);
             }else{
                 $processed.=$char;
             }
         }
+        
         // calculating the length
         $length = 0;
         while(isset($processed[$length])){
             $length++;
         }
-        // reversing the word
-        $reversed="";
+
+        // reversing
+        $reversed = '';
+        for ($i = $length - 1; $i >= 0; $i--) {
+            $reversed .= $processed[$i];
+        }
+        
         for ($i = 0; $i<$length; $i++){
             if($processed[$i] !== $reversed[$i]){
                 $response=[];
                 $response['status'] = 'Failed';
-                $response['massege'] = 'not a palondrome';
-                echo json_encode($response);
+                $response['message'] = 'not a palondrome';
+                return false;
             }
         }
-
+        return true;
+    }
+    if (checker($word)) {
+        echo json_encode(["status" => "success", "message" => "It is a palindrome"]);
+    } else {
+        echo json_encode(["status" => "failed", "message" => "Not a palindrome"]);
     }
 }
