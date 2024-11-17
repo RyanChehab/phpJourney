4987691341ee02f3917a641f41fe88fc79789abb
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 03:35:51 +0200

    php <=> defined class linkedlist

--- LL.php
+++ LL.php
@@ -8,3 +8,27 @@ Class Node{
         $this->next = null;
     }
 }
+
+Class LinkedList{
+    public $head; 
+
+    public function __construct(){
+        $this->head = null;
+    }
+
+    public function addNode($value){
+        $newNode = new Node($value);
+    
+        if ($this->head === null){
+            $this->head = $newNode;
+        }
+        else{
+            $current = $this->head;
+            while(current->next !==null){
+                $current= $current->$next;
+            }
+            }
+        $current->next = $newNode;
+    }
+}
+
