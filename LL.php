ca889fe279192822a7af2d2ee506ff2f31b26ad8
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 03:42:37 +0200

    php <=> added count vowels helper function

--- LL.php
+++ LL.php
@@ -30,5 +30,19 @@ Class LinkedList{
             }
         $current->next = $newNode;
     }
+    
+    private function countVowels($string) {
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+        $count = 0;
+    
+        foreach (str_split(strtolower($string)) as $char) {
+            if (in_array($char, $vowels)) {
+                $count++;
+            }
+        }
+    
+        return $count;
+    }
+
 }
 
