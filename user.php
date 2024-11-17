b953667f6ea4960cd4ecc10f5c390e99d47a17de
RyanChehab <ryanchehab20@gmail.com>
2024-11-17 16:12:55 +0200

    php <=> built checkPassword function

--- user.php
+++ user.php
@@ -6,5 +6,22 @@ class user {
         $this->id =$id;
     }
 
+    public function checkPassowrd($password){
+        if(strlen($password) < 12){
+            return "Password must be at least 12 char";
+        }
 
+        if(!preg_match('/[A-Z]/',$password)){
+            return "Password must include at least one uppercase letter.";
+        }
+
+        if(!preg_match('/[a-z]/',$password)){
+            return "Password must include at least one lower case letter";
+        }
+
+        if (!preg_match('/[\W_]/', $password)) { 
+            return "Password must include at least one special character.";
+        }
+        return "password is valid";
+    }
 }
